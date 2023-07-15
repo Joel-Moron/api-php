3adc66fe207a12085e8d70559f70a766d8494472
Joel Moron <121132208+Joel-Moron@users.noreply.github.com>
2023-07-14 19:46:50 -0500

    Update and rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+    require_once "./class/conexion/conexion.php";
+
+    $conexion = new conexion;
+
+    echo 'esto es una el index'
+
+/*     $query = "Select * from usuario ";
+
+    print_r(json_encode($conexion->obtenerDatos($query)));  */
+?>
